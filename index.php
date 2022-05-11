77bf2660346c9af2fab4ede157483d49f6fd7247
Frnkp21 <91593786+Frnkp21@users.noreply.github.com>
2022-05-11 12:05:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<html>
+<body>
+<h1>La historia </h1>
+
+Esto era una vez 3 hermanos cerditos El flautista, el violinista y el practico tu eres el <?php echo $_POST["cerdito"];?> de color <?php echo $_POST["colorcerdito"];?> un dia tus hermanos decidieron enviarte a comprar materiales para construir una casa y
+de camino te econtraste con un lobo <?php echo $_POST["ColorLobo"];?> que estaba entre dos caminos el de la derecha y el de la izquierda para continuar tu camino debias elejir camino y elejiste el de la  <?php echo $_POST["camino"]; ?>(si elejiste el de la derecha sigue leyendo aqui si no ves al apartado que pone otro camino).<br>
+Una vez fuiste por el de la derecha el lobo comenzo a segirte sin que te des cuenta, al rato una abuelita se puso a gritar que necesitaba ayuda y decidiste <?php echo $_POST["abuelita"];?>(Si elejiste seguir tu camino sigue leyendo aqui si no ves al apartado de abuelita) decidiste seguir tu camino  pero viste que la abuelita dejo de gritar y asomaste a verla y viste como metio a un animal dentro de un caldero gigante y se lo comia en ese momento te sentiste aliviado por no haber ido a ayudarla.
+cuando llegaste a donde vendian los materiales los compraste y te metiste un sprin de vuelta a casa sin hablar con nadie y sin hablar con nadie construiste tu casa de <?php echo $_POST["materialcasa"];?> y mientras la construias te clavaste un clavo en el pecho vino el lobo que te estaba siguiendo se comio a tus hermanos, moriste desangrado y tus hermanos devorados FIN.
+
+
+
+
+
+
+
+<h2> camino de la izquierda </h2>
+
+Decidiste ir por el de la izquierda  y al rato escuchaste una manada de lobos  y decidiste irte corriendo y como corrias muy rapido caiste por un barranco y moriste FIN.
+
+<h2> Abuelita </h2>
+
+fuiste a ayudar a la abuelita  le pregunstaste que le pasaba y te dijo que la puerta no le cerraba si podias ayudarla desde dentro y cuando entraste te encerro y subio la temperatura de la casa que realmente era un caldero gigante porque la abuelita era una bruja terminaste cocinado y en la barriga de una abuela bruja FIN.
+
+
+</body>
+</html>
+<!DOCTYPE HTML>
+<html LANG="es">
+<from>
+
+</form>
+</body>
+</html>
